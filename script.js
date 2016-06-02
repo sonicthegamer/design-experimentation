1df93c3b01042d295ea548370115ad1f7eef3af8
sonicthegamer <joelcolon1337@Gmail.com>
2016-06-02 17:36:01 +0000

    testing inputs

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
-
+/*
 var i = 0,
 fade = setInterval(function(){
     i++;
-    $('body').css('background','rgb(' + i + ',' + i + ',' + i + ')');
+    $('body').css('background','rgb(' + i + ',' + i + ',' + 100 + ')');
+},200);
+*/
 
-},20);
+value = $('#test').val();
 
 
+setInterval(function(){
+$('body').append('<h1>' + value + '</h1>');
+$('#string').text(value);
+
+},1000);
 
 });
